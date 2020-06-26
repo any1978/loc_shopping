100f5c7f7cb99e24f313ced8e69275b947b4d93c
katsuhiro.tsukinoki@gmail.com <slowandlow3104>
2020-06-26 21:29:01 +0900

    WIP seedデータ追加中

--- db/seeds.rb
+++ db/seeds.rb
@@ -6,26 +6,61 @@
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
 
-20.times do |n|
-  last_name = Faker::Name.last_name
-  first_name = Faker::Name.first_name
-  email = "example-#{n+1}@railstutorial.org"
-  password = "password"
-  postcode = Faker::Address.postcode
-  prefecture_code = Faker::Address.state
-  address_city = Faker::Address.city_prefix
-  address_street = Faker::Address.state_abbr
-  phone_number = Faker::PhoneNumber.phone_number
+# 20.times do |n|
+#   last_name = Faker::Name.last_name
+#   first_name = Faker::Name.first_name
+#   email = "example-#{n+1}@railstutorial.org"
+#   password = "password"
+#   postcode = Faker::Address.postcode
+#   prefecture_code = Faker::Address.state
+#   address_city = Faker::Address.city_prefix
+#   address_street = Faker::Address.state_abbr
+#   phone_number = Faker::PhoneNumber.phone_number
 
-  User.create!(first_name: first_name,
-              last_name: last_name,
-              email: email,
-              password: password,
-              password_confirmation: password,
-              prefecture_code: prefecture_code,
-              address_city: address_city,
-              address_street: address_street,
-              phone_number: phone_number,
+#   User.create!(first_name: first_name,
+#               last_name: last_name,
+#               email: email,
+#               password: password,
+#               password_confirmation: password,
+#               prefecture_code: prefecture_code,
+#               address_city: address_city,
+#               address_street: address_street,
+#               phone_number: phone_number,
+#   )
+# end
+
+require "csv"
+CSV.foreach('db/seeds/csv/user.csv', headers: true) do |row|
+  User.create(
+    id: row['id'],
+    first_name: row['first_name'],
+    last_name: row['last_name'],
+    email: row['email'],
+    password: row['password'],
+    password_confirmation: row['password'],
+    postcode: row['postcode'],
+    prefecture_code: row['prefecture_code'],
+    address_city: row['address_city'],
+    address_street: row['address_street'],
+    phone_number: row['phone_number']
+  )
+end
+
+
+CSV.foreach('db/seeds/csv/shop_owner.csv', headers: true) do |row|
+  ShopOwner.create(
+    id: row['id'],
+    first_name: row['first_name'],
+    last_name: row['last_name'],
+    email: row['email'],
+    password: row['password'],
+    password_confirmation: row['password'],
+    postcode: row['postcode'],
+    prefecture_code: row['prefecture_code'],
+    address_city: row['address_city'],
+    address_street: row['address_street'],
+    phone_number: row['phone_number'],
+    number_of_store: row['number_of_store']
   )
 end
 
